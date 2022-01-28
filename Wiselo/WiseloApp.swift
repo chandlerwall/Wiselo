e0613e94e917e0a6cf12320becc275c2cf281da9
Chandler Wall <c@swall.us>
2022-01-28 17:39:15 -0600

    Initial commit - Xcode new project

---
+++ Wiselo/WiseloApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiseloApp.swift
+//  Wiselo
+//
+//  Created by Chandler Wall on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct WiseloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
