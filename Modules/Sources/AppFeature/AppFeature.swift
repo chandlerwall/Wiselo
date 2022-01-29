8fb765effe1f3535b720f0b963dc1cd71a0218d1
Chandler Wall <c@swall.us>
2022-01-28 19:05:16 -0600

    wip - feature scaffolding; cleanup

--- Modules/Sources/AppFeature/AppFeature.swift
+++ Modules/Sources/AppFeature/AppFeature.swift
@@ -1,6 +1,27 @@
-public struct AppFeature {
-    public private(set) var text = "Hello, World!"
+import ComposableArchitecture
 
-    public init() {
+public struct AppFeatureState: Equatable {
+
+    public init() { }
+
+    let welcomeMessage: String = "Hello!"
+}
+
+public enum AppFeatureAction: Equatable {
+    case first
+}
+
+public struct AppEnvironment {
+    public init() { }
+}
+
+public let appReducer: Reducer<AppFeatureState, AppFeatureAction, AppEnvironment> = Reducer.combine(
+)
+
+private let appReducerCore: Reducer<AppFeatureState, AppFeatureAction, AppEnvironment> = Reducer
+{ state, action, environment in
+    switch action {
+    default:
+        return .none
     }
 }
---
+++ Modules/Sources/AppFeature/AppView.swift
@@ -0,0 +1,31 @@
+import ComposableArchitecture
+import SwiftUI
+
+public struct AppView: View {
+
+    struct ViewState: Equatable {
+        let welcomeMessage: String
+
+        init(state: AppFeatureState) {
+            self.welcomeMessage = state.welcomeMessage
+        }
+    }
+
+    let store: Store<AppFeatureState, AppFeatureAction>
+    @ObservedObject var viewStore: ViewStore<ViewState, AppFeatureAction>
+
+    public init(
+        store: Store<AppFeatureState, AppFeatureAction>
+    ) {
+        self.store = store
+        self.viewStore = ViewStore(self.store.scope(state: ViewState.init(state:)))
+    }
+
+    public var body: some View {
+        Text(self.viewStore.welcomeMessage)
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+// FIXME: Preview.
